0d4dd96b972849c0554a252b90ff7669624f6ecb
Anas sarkiz <72264380+AnasSarkiz@users.noreply.github.com>
2025-12-24 22:25:21 +0200

    feat(home): introduce PCBBurn landing page with tscircuit branding, routing, and Cosmos testing (#2)
    * feat(home): introduce PCBBurn landing page with tscircuit branding, routing, and Cosmos testing
    
    * fixed vercel deployment issue
    
    * resolve Vercel preview blank page and CI test failures

---
+++ examples/Landingpage.fixture.tsx
@@ -0,0 +1,9 @@
+import "../src/index.css"
+import { BrowserRouter as Router } from "react-router-dom"
+import LandingPage from "../lib/components/LandingPage"
+
+export default (
+  <Router>
+    <LandingPage />
+  </Router>
+)
